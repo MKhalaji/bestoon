d95c1b8cd5ac5bf477974ee836bebf1833c97332
MKhalaji <khalaji_1368@yahoo.com>
2019-09-16 05:38:24 -0700

    user creation works

--- web/urls.py
+++ web/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path(r'submit/expense/',views.submit_expense,name="submit_expense"),
     path(r'submit/income/',views.submit_income,name="submit_income"),
+    path(r'accounts/register/',views.register,name="register"),
 ]
 
 
